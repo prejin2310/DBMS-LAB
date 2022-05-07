b40ec17fb52df23893b7e51b81ee6dfc0bcb4a04
Prejin Senan <93463606+prejin2310@users.noreply.github.com>
2022-05-07 20:03:38 +0530

    create tables and describe table structure

--- CO2/1/create-table.sql
+++ CO2/1/create-table.sql
@@ -4,71 +4,55 @@ create table employee(empid int primary key,name varchar(20),cmpid int,desig var
 create table staff(stfid int primary key,name varchar(20),depid int, desig varchar(20),salary int,city varchar(20),foreign key(depid) references department(depid));
 create table students(stdid int,name varchar(20),class varchar(20),city varchar(20),tmarks int,per int,depid int,foreign key(depid) references department(depid));
 
-insert into department values(1001,'MCA');
-insert into department values(1002,'MBA');
-insert into department values(1003,'Adm-Office');
+desc department;
 
-mysql> select * from department;
-
- depid  name       
- ------ ------------
-  1001  MCA        
-  1002  MBA        
-  1003  Adm-Office 
+ Field  Type         Null  Key  Default  Extra 
+-----------------------------------------------
+ depid  int          NO    PRI  NULL           
 
 
+ desc company;
 
-insert into company values(8001,'MCET','Trivandrum',8);
-insert into company values(8002,'IT Solutions','Eranakulam',5);
-insert into company values(8003,'JS Gold','Aluva',3);
-
-mysql> select * from company;
-
- cmpid  name          city        no_emp 
------------------------------------------
-  8001  MCET          Trivandrum       8 
-  8002  IT Solutions  Eranakulam       5 
-  8003  JS Gold       Aluva            3 
+ Field   Type         Null  Key  Default  Extra 
+------------------------------------------------
+ cmpid   int          NO    PRI  NULL           
+ name    varchar(20)  YES        NULL           
+ city    varchar(20)  YES        NULL           
+ no_emp  int          YES        NULL           
 
 
-insert into employee values(5001,'Arun',8002,'clerk',15000,'Eranakulam');
-insert into employee values(5002,'Deepa',8001,'Ass-Prof',32000,'Trivandrum');
-insert into employee values(5003,'Jasmine',8003,'Salesgirl',12000,'Aluva');
+ desc employee;
 
-mysql> select * from employee;
-
- empid  name     cmpid  desig      salary  city       
-------------------------------------------------------
-  5001  Arun      8002  clerk       15000  Eranakulam 
-  5002  Deepa     8001  Ass-Prof    32000  Trivandrum 
-  5003  Jasmine   8003  Salesgirl   12000  Aluva      
-
-
-insert into students values(001,'Akshay','MCA','Trivandum',300,100,1001);
-insert into students values(002,'Arunima','MBA','Kollam',270,95,1002);
-insert into students values(003,'Krish','MCA','Trivandrum',200,83,1001);
-insert into students values(004,'Nevil','MBA','Aluva',150,50,1002);
-
-mysql> select * from student;
-
- stdid  name     class  city        tmarks  per   depid 
---------------------------------------------------------
-     1  Akshay   MCA    Trivandum      300   100   1001 
-     2  Arunima  MBA    Kollam         270    95   1002 
-     3  Krish    MCA    Trivandrum     200    83   1001 
-     4  Nevil    MBA    Aluva          150    50   1002 
+ Field   Type         Null  Key  Default  Extra 
+------------------------------------------------
+ empid   int          NO    PRI  NULL           
+ name    varchar(20)  YES        NULL           
+ cmpid   int          YES   MUL  NULL           
+ desig   varchar(25)  YES        NULL           
+ salary  int          YES        NULL           
+ city    varchar(20)  YES        NULL           
 
 
-insert into staff values(5002,'Deepa',1001,'Ass-Prof',32000,'Trivandrum');
-insert into staff values(6001,'Samuel',1001,'Ass-Prof',31000,'Kollam');
-insert into staff values(6002,'Aleena',1002,'Prof',38000,'Aluva');
-insert into staff values(6003,'Rojin',1003,'clerk',18000,'Aluva');
+ desc staff;
 
-mysql> select * from staff;
+ Field   Type         Null  Key  Default  Extra 
+------------------------------------------------
+ stfid   int          NO    PRI  NULL           
+ name    varchar(20)  YES        NULL           
+ depid   int          YES   MUL  NULL           
+ desig   varchar(20)  YES        NULL           
+ salary  int          YES        NULL           
+ city    varchar(20)  YES        NULL           
 
- stfid  name    depid  desig     salary  city       
-----------------------------------------------------
-  5002  Deepa    1001  Ass-Prof   32000  Trivandrum 
-  6001  Samuel   1001  Ass-Prof   31000  Kollam     
-  6002  Aleena   1002  Prof       38000  Aluva      
-  6003  Rojin    1003  clerk      18000  Aluva      
+
+ desc student;
+
+ Field   Type         Null  Key  Default  Extra 
+------------------------------------------------
+ stdid   int          YES        NULL           
+ name    varchar(20)  YES        NULL           
+ class   varchar(20)  YES        NULL           
+ city    varchar(20)  YES        NULL           
+ tmarks  int          YES        NULL           
+ per     int          YES        NULL           
+ depid   int          YES   MUL  NULL           
